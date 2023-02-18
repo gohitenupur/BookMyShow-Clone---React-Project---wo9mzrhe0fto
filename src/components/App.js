179fb1733e6df2d447ebfc952d574576ee947b19
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-02-18 15:41:21 +0530

    update1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SingleMovie from "./pages/SingleMovie";
 const App = () => {
-
-
   return (
-    <div id="main">
-    </div>
-  )
-}
-
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie:id" element={<SingleMovie />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/pages/SingleMovie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleMovie = () => {
+  return (
+    <div>
+      <h1>SingleMovre</h1>
+    </div>
+  );
+};
+
+export default SingleMovie;
